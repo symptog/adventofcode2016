b60d1a358184079996aa84ff6f8c2974684b71a0
Tobias Gall <tobias.gall@mailbox.org>
2016-12-11 13:49:49 +0100

    9. Dezember Teil 2 (not working)

--- 9.py
+++ 9.py
@@ -26,3 +26,27 @@ while i < len(file):
 
 print(len(new_file))
 
+#2
+def get_length(text, i=0):
+    if '(' not in text:
+        return len(text)
+
+    while i < len(text):
+        if text[i] != '(':
+            i += 1
+        else:
+            marker_start = i
+            while text[i] != ')':
+                i += 1
+            i += 1
+            marker_end = i
+            marker = text[marker_start:marker_end]
+            chars, times = [int(x) for x in marker.strip('()').split('x')]
+            s = text[i:i + chars]
+            ss = []
+            for _ in range(times):
+                ss.append(s)
+            s_new = ''.join(ss)
+            return get_length(s_new) + get_length(text[len(s):])
+
+print(get_length(file))
